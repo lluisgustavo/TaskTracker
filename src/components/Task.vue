5c2d1b72d994f33ecd6599bbec189a01aad33bc9
Luis Sousa <luis.sousa@giftconsult.com.br>
2021-07-21 16:26:45 -0300

    Toggle Reminder

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template> 
-    <div :class="[task.reminder ? 'reminder' : '', 'task']">
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
         <h3>{{ task.text }}
-        <i @click="onDelete(task.id)" class="fas fa-times"></i></h3> 
+        <i @click="$emit('delete-task', id)" class="fas fa-times"></i></h3> 
         <p>{{ task.day }}</p>
     </div>
 </template>
@@ -11,12 +11,7 @@ export default {
     name: 'Task',
     props: {
         task: Object
-    },
-    methods: {
-        onDelete(id) { 
-            this.$emit('delete-task', id)
-        }
-    }
+    }, 
 }
 </script>
  
