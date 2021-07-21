0e9459a1cd9a4ffa157d0da9b4fb248a6387794d
Luis Sousa <luis.sousa@giftconsult.com.br>
2021-07-21 16:19:42 -0300

    Delete Task

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template> 
     <div :class="[task.reminder ? 'reminder' : '', 'task']">
         <h3>{{ task.text }}
-        <i class="fas fa-times"></i></h3> 
+        <i @click="onDelete(task.id)" class="fas fa-times"></i></h3> 
         <p>{{ task.day }}</p>
     </div>
 </template>
@@ -11,6 +11,11 @@ export default {
     name: 'Task',
     props: {
         task: Object
+    },
+    methods: {
+        onDelete(id) { 
+            this.$emit('delete-task', id)
+        }
     }
 }
 </script>
